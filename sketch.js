06ff3f1bd00fd79d59d15a92dab5cef4d8a38f35
Prof Ju & Alunos <90655877+prof-Ju@users.noreply.github.com>
2022-08-01 14:08:43 -0300

    alteraçõooes

--- sketch.js
+++ sketch.js
@@ -1,6 +1,16 @@
 var bg;
 var player;
 var quadras;
+
+var gameState = 0;
+
+// 0 = menu
+// 1 =  escolha o carro
+// 2 = historinha
+// 3 = play
+
+var title, input, playerName, playButton, displayName;
+
 function preload() {
     bg = loadImage("./assests/bg.jpeg");
 }
@@ -13,24 +23,59 @@ function setup() {
     quadras = new Group();
 
     geradorQuadras();
+
+
+    // criação do "menu"
+    title = createElement("h1","Corrida de Carros");
+    title.class("title");
+
+    input =  this.input = createInput("").attribute("placeholder", "Digite Seu Nome");;
+    input.class("customInput");
+
+    playButton = createButton("play");
+    playButton.class("customButton");
+    playButton.mouseClicked(()=>{
+        gameState = 1;
+    })
+
+    displayName = createElement("h3");
+    displayName.class("display");
+
 }
 
 function draw() {
     background("black");
-    image(bg, 0,0,width*3, height*3);
+   
 
-     console.log("player: "+player.x, player.y)
+     //console.log("player: "+player.x, player.y)
     // console.log(camera.position.x, camera.position.y)
 
 //ajustando a camera do jogo!
-    camera.position.x =  player.x ;
-    camera.position.y =  player.y ;
+    
     
 
-    player.collide(quadras)
+   
 
-    drawSprites();
-    controls();
+
+    if(gameState == 0 ){
+        playerName = input.value();
+        //console.log(playerName)
+    }
+
+    if(gameState == 1){
+        hideMenu();
+        displayName.html(playerName);
+        image(bg, 0,0,width*3, height*3);
+        player.collide(quadras);
+
+        camera.position.x =  player.x ;
+        camera.position.y =  player.y ;
+
+        drawSprites();
+        controls();
+    }
+
+   
 }
 
 
@@ -67,5 +112,11 @@ function geradorQuadras(){
     quadras.add(quadra3);
 
     var quadra4 = createSprite(1640, 2010, 380, 30)
-    //quadras.add(quadra4);
+    quadras.add(quadra4);
+}
+
+function hideMenu(){
+    playButton.hide();
+    title.hide();
+    input.hide();
 }
