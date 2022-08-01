a7d878227b1ebf55029820948e003f8106cd86a7
Prof Ju & Alunos <90655877+prof-Ju@users.noreply.github.com>
2022-08-01 14:50:26 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 var bg;
 var player;
-var quadras;
+var quadras, policiais;
 
-var gameState = 0;
-
+var gameState = 1;
+var chegada;
 // 0 = menu
 // 1 =  escolha o carro
 // 2 = historinha
@@ -17,13 +17,23 @@ function preload() {
 
 function setup() {
     createCanvas(824, 768);
-    player = createSprite(1870, 1920, 10,10);
+
+    chegada = createSprite(3000,890,100,100);
+    chegada.shapeColor = "green";
+
+    player = createSprite(250, 350, 10,10);
     player.shapeColor = "red"
 
     quadras = new Group();
+    policiais = new Group();
 
-    geradorQuadras();
+    geradorQuadras(10,200);
+    geradorQuadras(10,400);
+    geradorQuadras(10,600);
+    geradorQuadras(10,800);
 
+    geradorPolicia();
+   
 
     // criação do "menu"
     title = createElement("h1","Corrida de Carros");
@@ -47,15 +57,9 @@ function draw() {
     background("black");
    
 
-     //console.log("player: "+player.x, player.y)
+    console.log("player: " +player.x, player.y)
     // console.log(camera.position.x, camera.position.y)
-
-//ajustando a camera do jogo!
-    
-    
-
    
-
 
     if(gameState == 0 ){
         playerName = input.value();
@@ -65,54 +69,96 @@ function draw() {
     if(gameState == 1){
         hideMenu();
         displayName.html(playerName);
-        image(bg, 0,0,width*3, height*3);
+        //image(bg, 0,0,width*3, height*3);
         player.collide(quadras);
+        policiais.collide(quadras);
 
         camera.position.x =  player.x ;
         camera.position.y =  player.y ;
+
+        if(player.collide(chegada)){
+            gameState = 2
+        }
+
+        if(player.isTouching(policiais)){
+            gameState = 3
+        }
+
 
         drawSprites();
         controls();
     }
 
+    if(gameState == 2) {
+        fill("green");
+        text("parabéns! "+ playerName +" Você ganhou!", player.x + 20, player.y+ 20);
+    }
+   
+    if(gameState == 3) {
+        fill("red");
+        text(playerName +", Você perdeu :(", player.x + 20, player.y+ 20);
+
+    }
    
 }
 
 
 function controls(){
     if (keyDown(LEFT_ARROW)){
-        player.x -= 5;
+        player.x -= 10;
     }
     if (keyDown(RIGHT_ARROW)){
-        player.x += 5;
+        player.x += 10;
     }
     if (keyDown(UP_ARROW)){
-        player.y -= 5;
+        player.y -= 10;
     }
     if (keyDown(DOWN_ARROW)){
-        player.y += 5;
+        player.y += 10;
     }
 }
 
 
-function geradorQuadras(){
-    var quadra1 = createSprite(1232,2245, 707,500);
-    quadra1.shapeColor = "blue";
-    quadras.add(quadra1);
+function geradorQuadras(numberOfSquares,y){
+//     var quadra1 = createSprite(1232,2245, 707,500);
+//     quadra1.shapeColor = "blue";
+//     quadras.add(quadra1);
 
-   var quadra2 = createSprite(1816, 2010, 380, 30)
-   quadra2.shapeColor = "blue";
-    quadras.add(quadra2);
+//    var quadra2 = createSprite(1816, 2010, 380, 30)
+//    quadra2.shapeColor = "blue";
+//     quadras.add(quadra2);
     
    
-   // quadras2.rotation = quadras2.rotation + 33
+//    // quadras2.rotation = quadras2.rotation + 33
 
-    var quadra3 = createSprite(1640, 2200, 30, 380)
-    quadra3.shapeColor = "blue";
-    quadras.add(quadra3);
+//     var quadra3 = createSprite(1640, 2200, 30, 380)
+//     //quadra3.shapeColor = "blue";
+//     quadras.add(quadra3);
 
-    var quadra4 = createSprite(1640, 2010, 380, 30)
-    quadras.add(quadra4);
+//     var quadra4 = createSprite(1640, 2010, 380, 30)
+//     quadras.add(quadra4);
+
+
+
+    for(var i = 0; i < numberOfSquares; i++){
+        var quadra = createSprite(200+300*i,y, 250,80);
+        quadra.shapeColor = "blue";
+        quadras.add(quadra);
+    }
+}
+
+function geradorPolicia(){
+    for(var i = 0; i < 8; i++){
+        var x = random(200, 300);
+        var y = random(200, 890);
+        var policia = createSprite(x,y, 30,30);
+        var speeds = [-8,8];
+        policia.velocityY = random(speeds)
+        policia.velocityX = random(speeds)
+        policia.shapeColor = "purple";
+
+        policiais.add(policia);
+    }
 }
 
 function hideMenu(){
